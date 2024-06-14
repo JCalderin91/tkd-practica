8de1fd2a4e5b0ff2f3ad58835e6e5bbe5be18048
Jesus Calderin <jesus.c@servicepad.com>
2024-06-14 10:12:43 -0400

    Update button hover color

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -8,7 +8,7 @@ defineProps({
 
 <template>
   <button
-    class="bg-blue-700 text-white rounded-md min-h-[32px] px-6 py-2 font-semibold hover:bg-gray-800 shadow-md active:shadow-none ease-in-out duration-300 shadow-slate-400 transition-all">
+    class="bg-blue-700 text-white rounded-md min-h-[32px] px-6 py-2 font-semibold hover:bg-blue-800 shadow-md active:shadow-none ease-in-out duration-300 shadow-slate-400 transition-all">
     <slot />
   </button>
 </template>
